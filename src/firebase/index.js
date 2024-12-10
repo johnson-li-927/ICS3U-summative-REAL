5493fa52224706efbd75d4c34bd5d35ce8c3570e
[johnson <[johnson.li6@student.tdsb.on.ca]>
2024-12-10 13:54:36 -0500

    second push

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
-
+const firebaseConfig = {
+    apiKey: "AIzaSyC9QehC4FrFHcoEyhR2Dj7XHLiWFUEDOro",
+    authDomain: "summative-717a1.firebaseapp.com",
+    projectId: "summative-717a1",
+    storageBucket: "summative-717a1.firebasestorage.app",
+    messagingSenderId: "74864377869",
+    appId: "1:74864377869:web:f3c019b84ad2dad5bd1544"
+  };
+  
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
